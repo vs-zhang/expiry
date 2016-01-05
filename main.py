6a6729d31ecaea0b5067c89830c1bf769ab010e4
Zhuoming Tang <tzmgle@gmail.com>
2016-01-04 22:46:24 -0500

    add .gitignore

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
+
+kivy.require('1.0.6')  # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.label import Label
 
 
 class MyApp(App):
-
     def build(self):
         return Label(text='Hello world')
 
